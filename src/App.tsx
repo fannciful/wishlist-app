4cd46cc5900c00148c1e17c4824d59df60c0a155
fannciful <juliya992@gmail.com>
2025-11-29 13:39:41 +0200

    made changes in main file

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { WishProvider } from "./context/WishContext";
-import { Dashboard } from "./components/Dashboard/Dashboard";
-import { WishPage } from "./components/WishPage/WishPage";
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { WishProvider } from './context/WishContext';
+import { Dashboard } from './components/Dashboard/Dashboard';
+import { WishPage } from './components/WishPage/WishPage';
 
 function App() {
   return (
     <Router>
       <WishProvider>
         <Routes>
-          <Route path="/" element={<Dashboard/ >} />
-           <Route path="/wish/:id" element={<WishPage/ >} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/wish/:id" element={<WishPage />} />
         </Routes>
       </WishProvider>
     </Router>
